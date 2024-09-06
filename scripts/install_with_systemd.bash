ffb0c13a1c3ec3f28b28ac09aa419645cef5f268
XIAO Shuqi <xiaosq2000@gmail.com>
2024-09-06 16:01:32 +0800

    Update

--- scripts/install_with_systemd.bash
+++ scripts/install_with_systemd.bash
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 # Be safe.
-set -euo pipefail
-
-PROTOCOL="trojan"
+set -eo pipefail
 
 # Logging
-print_debug=true
-print_verbose=true
-
 INDENT='    '
 BOLD="$(tput bold 2>/dev/null || printf '')"
 GREY="$(tput setaf 0 2>/dev/null || printf '')"
@@ -28,51 +23,74 @@ info() {
 	printf '%s\n' "${BOLD}${GREEN}INFO:${RESET} $*"
 }
 debug() {
-	printf '%s\n' "${BOLD}${GREY}DEBUG:${RESET} $*"
+	if [[ $VERBOSE == true ]]; then
+		printf '%s\n' "${BOLD}${GREY}DEBUG:${RESET} $*"
+	fi
+}
+completed() {
+	printf '%s\n' "${BOLD}${GREEN}✓${RESET} $*"
 }
 
 usage() {
-    printf "%s\n" \
-        "Usage: " \
-        "${INDENT}$0 [option]" \
-        ""
-    printf "%s\n" \
-        "Options: " \
-        "${INDENT}-h, --help                  Display help messeges" \
-        "${INDENT}-p, --protocol PROTOCOL     Specify which protocol to install" \
-        ""
+	printf "%s\n" \
+		"Usage: " \
+		"${INDENT}$0 [option]" \
+		""
+	printf "%s\n" \
+		"Options: " \
+		"${INDENT}-h, --help                  Display help messeges" \
+		"${INDENT}-V, --verbose               Debug logging" \
+		"" \
+		"${INDENT}-p, --protocol PROTOCOL     Specify which protocol to install:" \
+		"                                     ${SUPPORTED_PROTOCOL}" \
+		"${INDENT}--enable-now                " \
+		""
 }
 
+DEFAULT_PROTOCOL="trojan"
+SUPPORTED_PROTOCOL="trojan hysteria2"
+
 while [[ $# -gt 0 ]]; do
-    case "$1" in
-    -h | --help)
-        usage
-        exit 0
-        ;;
-    -p | --protocol)
-        PROTOCOL="$2"
-        shift 2
-        ;;
-    *)
-        error "Unknown argument: $1"
-        usage
-        ;;
-    esac
+	case "$1" in
+	-h | --help)
+		usage
+		exit 0
+		;;
+	-p | --protocol)
+		PROTOCOL="$2"
+		shift 2
+		;;
+	-V | --verbose)
+		VERBOSE=true
+		shift 1
+		;;
+	--enable-now)
+		ENABLE_NOW=true
+		shift 1
+		;;
+	*)
+		error "Unknown argument: $1"
+		usage
+		;;
+	esac
 done
 
-if [[ $PROTOCOL != "trojan" && $PROTOCOL != "hysteria2" ]]; then
-    error "$PROTOCOL is not supported yet."
-    exit 1
+if [[ -z $PROTOCOL ]]; then
+	PROTOCOL=$DEFAULT_PROTOCOL
 fi
 
-XDG_PREFIX_DIR="/usr/local"
-SERVICE_DIR="/etc/systemd/system"
-SERVICE_NAME="sing-box-${PROTOCOL}.service"
-SERVICE_FILE="${SERVICE_DIR}/${SERVICE_NAME}"
+good=$(
+	IFS=" "
+	for p in $SUPPORTED_PROTOCOL; do
+		if [[ "${p}" = "${PROTOCOL}" ]]; then
+			printf 1
+			break
+		fi
+	done
+)
 
-# Superuser privilege is required.
-if [[ $(id -u) -ne 0 ]]; then
-	error "The script needs root privilege to run. Try again with sudo."
+if [[ "${good}" != "1" ]]; then
+	error "$PROTOCOL is not supported yet. Supported protocols: ${SUPPORTED_PROTOCOL}"
 	exit 1
 fi
 
@@ -80,15 +98,32 @@ fi
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd ${script_dir}
 
-if [[ -f "${SERVICE_FILE}" ]]; then
-	debug "Detected pre-existed ${SERVICE_NAME} in $SERVICE_DIR."
-	debug "Stop and disable it."
-	sudo systemctl stop ${SERVICE_NAME} >/dev/null 2>&1
-	sudo systemctl disable ${SERVICE_NAME} >/dev/null 2>&1
-	debug "Replace it with ${SERVICE_NAME} in ${script_dir}."
-	sudo cp "${SERVICE_NAME}" "${SERVICE_FILE}"
+# Superuser privilege is required.
+if [[ $(id -u) -ne 0 ]]; then
+	error "The script needs root privilege to run. Try again with sudo."
+	exit 1
+fi
+
+XDG_PREFIX_DIR="/usr/local"
+SERVICE_DIR="/etc/systemd/system"
+SERVICE_NAME="sing-box-${PROTOCOL}.service"
+SERVICE_FILE="${SERVICE_DIR}/${SERVICE_NAME}"
+
+set +e
+SERVICE_FILES_TO_REMOVE=$(ls $SERVICE_DIR | grep 'sing-box')
+if [[ ! -z "${SERVICE_FILES_TO_REMOVE}" ]]; then
+	debug "Detected ${INDENT}${SERVICE_FILES_TO_REMOVE}. Stop, disable and remove."
+	sudo systemctl stop ${SERVICE_FILES_TO_REMOVE} >/dev/null 2>&1
+	sudo systemctl disable ${SERVICE_FILES_TO_REMOVE} >/dev/null 2>&1
+	sudo rm ${SERVICE_DIR}/${SERVICE_FILES_TO_REMOVE} >/dev/null 2>&1
+fi
+set -e
+
+if [[ -f "${script_dir}/${SERVICE_NAME}" ]]; then
+	sudo cp "${script_dir}/${SERVICE_NAME}" "${SERVICE_FILE}"
+	debug "${script_dir}/${SERVICE_NAME} ${GREEN}${BOLD}->${RESET} ${script_dir}."
 else
-	sudo cp "${SERVICE_NAME}" "${SERVICE_FILE}"
+	error "${script_dir}/${SERVICE_NAME} is not found. Contact your admin."
 fi
 
 sudo mkdir -p "${XDG_PREFIX_DIR}/bin"
@@ -96,32 +131,41 @@ sudo mkdir -p "${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}"
 sudo mkdir -p "/var/lib/sing-box/"
 
 if [[ -f "sing-box" ]]; then
-	sudo cp ./sing-box "${XDG_PREFIX_DIR}/bin/sing-box"
+	sudo cp ${script_dir}/sing-box "${XDG_PREFIX_DIR}/bin/sing-box"
+	debug "${script_dir}/sing-box ${GREEN}${BOLD}->${RESET} ${XDG_PREFIX_DIR}/bin/sing-box"
 else
-	error "Executable binary file, sing-box, is not found."
+	error "${script_dir}/sing-box is not found."
 	exit 1
 fi
 
-if [[ -f "${PROTOCOL}-client.json" ]]; then
-	sudo cp "${PROTOCOL}-client.json" "${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
-	debug "Copy ${PROTOCOL}-client.json to ${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
-elif [[ -f "${PROTOCOL}-server.json" ]]; then
-	sudo cp "${PROTOCOL}-server.json" "${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
-	debug "Copy ${PROTOCOL}-server.json to ${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
+if [[ -f "${script_dir}/${PROTOCOL}-client.json" ]]; then
+	sudo cp "${script_dir}/${PROTOCOL}-client.json" "${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
+	debug "${script_dir}/${PROTOCOL}-client.json ${GREEN}${BOLD}->${RESET} ${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
+elif [[ -f "${script_dir}/${PROTOCOL}-server.json" ]]; then
+	sudo cp "${script_dir}/${PROTOCOL}-server.json" "${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
+	debug "${script_dir}/${PROTOCOL}-server.json ${GREEN}${BOLD}->${RESET} ${XDG_PREFIX_DIR}/etc/sing-box/${PROTOCOL}/config.json"
 fi
 
-debug "Systemd Daemon reload."
+debug "Systemd Daemon Reload."
 sudo systemctl daemon-reload >/dev/null 2>&1
-debug "Systemd Enable now."
-sudo systemctl enable --now ${SERVICE_NAME} >/dev/null 2>&1
+if [[ "$ENABLE_NOW" == "true" ]]; then
+	debug "Systemd Enable Now."
+	sudo systemctl enable --now ${SERVICE_NAME} >/dev/null 2>&1
+fi
 
-info "The networking proxy service, sing-box (${PROTOCOL}), will be managed by systemd from now on.
+completed "${BOLD}sing-box (${PROTOCOL})${RESET} is successfully installed and managed by systemd from now on.
 
-    Try: 
+Try: 
+${INDENT}${GREEN}${BOLD}\$${RESET} sudo systemctl status ${SERVICE_NAME}
 
-    ${INDENT}${GREEN}${BOLD}\$${RESET} sudo systemctl status ${SERVICE_NAME}
+To use some handy commands:
+${INDENT}${GREEN}${BOLD}\$${RESET} source ${script_dir}/setup.bash
+${INDENT}${GREEN}${BOLD}\$${RESET} set_proxy
+${INDENT}${GREEN}${BOLD}\$${RESET} unset_proxy
 
-    To use some handy commands:
+You can add 
 
-    ${INDENT}${GREEN}${BOLD}\$${RESET} source ${script_dir}/setup.bash
-    "
+${INDENT}source ${script_dir}/setup.bash
+
+into ~/.bashrc.
+"
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,101 +1,100 @@
 # !/usr/bin/env bash
-print_debug=false
-print_verbose=true
 
-INDENT='  '
+PROTOCOL=trojan
+VERBOSE=false
 
-RESET=$(tput sgr0)
-BOLD=$(tput bold)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-BLUE=$(tput setaf 4)
-PURPLE=$(tput setaf 5)
-CYAN=$(tput setaf 6)
+set -euo pipefail
 
+INDENT='    '
+BOLD="$(tput bold 2>/dev/null || printf '')"
+GREY="$(tput setaf 0 2>/dev/null || printf '')"
+UNDERLINE="$(tput smul 2>/dev/null || printf '')"
+RED="$(tput setaf 1 2>/dev/null || printf '')"
+GREEN="$(tput setaf 2 2>/dev/null || printf '')"
+YELLOW="$(tput setaf 3 2>/dev/null || printf '')"
+BLUE="$(tput setaf 4 2>/dev/null || printf '')"
+MAGENTA="$(tput setaf 5 2>/dev/null || printf '')"
+RESET="$(tput sgr0 2>/dev/null || printf '')"
 error() {
-    printf "${RED}${BOLD}ERROR:${RESET} %s\n" "$@" >&2
-    return 1;
+	printf '%s\n' "${BOLD}${RED}ERROR:${RESET} $*" >&2
 }
 warning() {
-    printf "${YELLOW}${BOLD}WARNING:${RESET} %s\n" "$@" >&2
-    return 1;
+	printf '%s\n' "${BOLD}${YELLOW}WARNING:${RESET} $*"
 }
 info() {
-    printf "${GREEN}${BOLD}INFO:${RESET} %s\n" "$@"
-    return 0;
+	printf '%s\n' "${BOLD}${GREEN}INFO:${RESET} $*"
 }
 debug() {
-    if [[ $print_debug == "true" ]]; then
-        printf "${BOLD}DEBUG:${RESET} %s\n" "$@"
-    fi
+	if [[ $VERBOSE == true ]]; then
+		printf '%s\n' "${BOLD}${GREY}DEBUG:${RESET} $*"
+	fi
 }
 
 check_public_ip() {
-    local ipinfo=$(curl --silent ipinfo.io)
-    if [[ -z "$ipinfo" ]]; then
-        error "No public networking."
-    else
-        echo -e "${PURPLE}Public Network:${RESET}\n${INDENT}$(echo "$ipinfo" | grep --color=never -e '\"ip\"' -e '\"city\"' | sed 's/^[ \t]*//' | awk '{print}' ORS=' ')"
-    fi
-    echo
+	local ipinfo=$(curl --silent ipinfo.io)
+	if [[ -z "$ipinfo" ]]; then
+		error "No public networking."
+	else
+		echo -e "${MAGENTA}Public Network:${RESET}\n${INDENT}$(echo "$ipinfo" | grep --color=never -e '\"ip\"' -e '\"city\"' | sed 's/^[ \t]*//' | awk '{print}' ORS=' ')"
+	fi
+	echo
 }
 check_private_ip() {
-    echo -e "${PURPLE}Private Network:${RESET}\n${INDENT}\"ip\": \"$(hostname -I | awk '{ print $1; }')\","
-    echo
+	echo -e "${MAGENTA}Private Network:${RESET}\n${INDENT}\"ip\": \"$(hostname -I | awk '{ print $1; }')\","
+	echo
 }
 
 set_proxy() {
-    if [[ $(uname -r | grep 'WSL2') ]]; then
-        warning "Make sure the VPN client is working on host."
-        local host="'$(cat /etc/resolv.conf | grep '^nameserver' | cut -d ' ' -f 2)'"
-        local port=1080
-    elif [ -f /.dockerenv ]; then
-        warning "It's a docker container. only \"host\" networking mode is supported."
-        local host="'127.0.0.1'"
-        local port=1080
-    else
-        if [[ $(lsb_release -d | grep 'Ubuntu') ]]; then
-            local host="'127.0.0.1'"
-            local port=1080
-            debug "Start the VPN client service."
-            sudo systemctl start sing-box.service
-            debug "Set GNOME networking proxy settings."
-            dconf write /system/proxy/mode "'manual'"
-            dconf write /system/proxy/http/host ${host}
-            dconf write /system/proxy/http/port ${port}
-            dconf write /system/proxy/https/host ${host}
-            dconf write /system/proxy/https/port ${port}
-            dconf write /system/proxy/ftp/host ${host}
-            dconf write /system/proxy/ftp/port ${port}
-            dconf write /system/proxy/socks/host ${host}
-            dconf write /system/proxy/socks/port ${port}
-            dconf write /system/proxy/ignore-hosts "'localhost,127.0.0.0/8,::1'"
-        else
-            error "This platform is not supported."
-        fi
-    fi
-    debug "Set environment variables and configure for specific programs."
-    local host="${host//\'/}"
-    local port="${port}"
-    export http_proxy=${http_proxy:-"${host}:${port}"}
-    export https_proxy=${https_proxy:-"${host}:${port}"}
-    export ftp_proxy=${ftp_proxy:-"${host}:${port}"}
-    export socks_proxy=${socks_proxy:-"${host}:${port}"}
-    export no_proxy=${no_proxy:-"localhost,127.0.0.0/8,::1"}
-    export HTTP_PROXY=${HTTP_PROXY:-${http_proxy}}
-    export HTTPS_PROXY=${HTTPS_PROXY:-${https_proxy}}
-    export FTP_PROXY=${FTP_PROXY:-${ftp_proxy}}
-    export SOCKS_PROXY=${SOCKS_PROXY:-${socks_proxy}}
-    export NO_PROXY=${NO_PROXY:-${no_proxy}}
-    debug "Set git global network proxy."
-    git config --global http.proxy ${http_proxy}
-    git config --global https.proxy ${https_proxy}
-    info "You're recommended to wait a couple of seconds until the VPN client is on.
+	if [[ $(uname -r | grep 'WSL2') ]]; then
+		warning "Make sure the VPN client is working on host."
+		local host="'$(cat /etc/resolv.conf | grep '^nameserver' | cut -d ' ' -f 2)'"
+		local port=1080
+	elif [ -f /.dockerenv ]; then
+		warning "It's a docker container. only \"host\" networking mode is supported."
+		local host="'127.0.0.1'"
+		local port=1080
+	else
+		if [[ $(lsb_release -d | grep 'Ubuntu') ]]; then
+			local host="'127.0.0.1'"
+			local port=1080
+			debug "Start the VPN client service."
+			sudo systemctl start sing-box-${PROTOCOL}.service
+			debug "Set GNOME networking proxy settings."
+			dconf write /system/proxy/mode "'manual'"
+			dconf write /system/proxy/http/host ${host}
+			dconf write /system/proxy/http/port ${port}
+			dconf write /system/proxy/https/host ${host}
+			dconf write /system/proxy/https/port ${port}
+			dconf write /system/proxy/ftp/host ${host}
+			dconf write /system/proxy/ftp/port ${port}
+			dconf write /system/proxy/socks/host ${host}
+			dconf write /system/proxy/socks/port ${port}
+			dconf write /system/proxy/ignore-hosts "'localhost,127.0.0.0/8,::1'"
+		else
+			error "This platform is not supported."
+		fi
+	fi
+	debug "Set environment variables and configure for specific programs."
+	local host="${host//\'/}"
+	local port="${port}"
+	export http_proxy=${http_proxy:-"${host}:${port}"}
+	export https_proxy=${https_proxy:-"${host}:${port}"}
+	export ftp_proxy=${ftp_proxy:-"${host}:${port}"}
+	export socks_proxy=${socks_proxy:-"${host}:${port}"}
+	export no_proxy=${no_proxy:-"localhost,127.0.0.0/8,::1"}
+	export HTTP_PROXY=${HTTP_PROXY:-${http_proxy}}
+	export HTTPS_PROXY=${HTTPS_PROXY:-${https_proxy}}
+	export FTP_PROXY=${FTP_PROXY:-${ftp_proxy}}
+	export SOCKS_PROXY=${SOCKS_PROXY:-${socks_proxy}}
+	export NO_PROXY=${NO_PROXY:-${no_proxy}}
+	debug "Set git global network proxy."
+	git config --global http.proxy ${http_proxy}
+	git config --global https.proxy ${https_proxy}
+	info "You're recommended to wait a couple of seconds until the VPN client is on.
 
       Try with:
 
-      ${INDENT}$ print_verbose=true check_proxy_status
+      ${INDENT}$ VERBOSE=true check_proxy_status
 
       or
 
@@ -103,35 +102,35 @@ set_proxy() {
     "
 }
 unset_proxy() {
-    if [[ ! $(uname -r | grep 'WSL2') && ! -f /.dockerenv ]]; then
-        if [[ $(lsb_release -d | grep 'Ubuntu') ]]; then
-            debug "Stop VPN client service."
-            sudo systemctl stop sing-box.service
-            debug "Unset GNOME networking proxy settings."
-            dconf write /system/proxy/mode "'none'"
-        else
-            error "Unsupported for this platform."
-        fi
-    fi
-    debug "Unset environment variables."
-    unset http_proxy
-    unset https_proxy
-    unset HTTP_PROXY
-    unset HTTPS_PROXY
-    unset ftp_proxy
-    unset FTP_PROXY
-    unset socks_proxy
-    unset SOCKS_PROXY
-    unset all_proxy
-    unset ALL_PROXY
-    unset no_proxy
-    unset NO_PROXY
-    debug "Unset git global network proxy."
-    git config --global --unset http.proxy
-    git config --global --unset https.proxy
-    info "Try with:
+	if [[ ! $(uname -r | grep 'WSL2') && ! -f /.dockerenv ]]; then
+		if [[ $(lsb_release -d | grep 'Ubuntu') ]]; then
+			debug "Stop VPN client service."
+			sudo systemctl stop sing-box-${PROTOCOL}.service
+			debug "Unset GNOME networking proxy settings."
+			dconf write /system/proxy/mode "'none'"
+		else
+			error "Unsupported for this platform."
+		fi
+	fi
+	debug "Unset environment variables."
+	unset http_proxy
+	unset https_proxy
+	unset HTTP_PROXY
+	unset HTTPS_PROXY
+	unset ftp_proxy
+	unset FTP_PROXY
+	unset socks_proxy
+	unset SOCKS_PROXY
+	unset all_proxy
+	unset ALL_PROXY
+	unset no_proxy
+	unset NO_PROXY
+	debug "Unset git global network proxy."
+	git config --global --unset http.proxy
+	git config --global --unset https.proxy
+	info "Try with:
 
-      ${INDENT}$ print_verbose=true check_proxy_status
+      ${INDENT}$ VERBOSE=true check_proxy_status
 
       or
 
@@ -139,34 +138,36 @@ unset_proxy() {
     "
 }
 check_proxy_status() {
-    local proxy_env=$(env | grep --color=never -i 'proxy')
-    if [[ -n $proxy_env ]]; then
-        info "The shell is using network proxy.";
-    else
-        info "The shell is ${BOLD}${YELLOW}NOT${RESET} using network proxy.";
-    fi
-    echo
-    check_public_ip;
-    if [[ $print_verbose == "true" ]]; then
-        echo "${CYAN}Environment Variables Related with Network Proxy: ${RESET}"
-        echo $proxy_env | while read line; do echo "${INDENT}${line}"; done
-        echo
-        echo "${YELLOW}VPN Client Status: ${RESET}"
-        if [[ $(uname -r | grep 'WSL2') ]]; then
-            warning "Unknown. For WSL2, the VPN client is probably running on the host machine. Please check manually.";
-        elif [[ -f /.dockerenv ]]; then
-            warning "Unknown. For a Docker container, the VPN client is probably running on the host machine. Please check manually.";
-        else
-            echo "${INDENT}$(systemctl is-active sing-box.service)"
-        fi
-        echo
-    fi
+	local proxy_env=$(env | grep --color=never -i 'proxy')
+	if [[ -n $proxy_env ]]; then
+		info "The shell is using network proxy."
+	else
+		info "The shell is ${BOLD}${YELLOW}NOT${RESET} using network proxy."
+	fi
+	echo
+	check_public_ip
+	if [[ $VERBOSE == "true" ]]; then
+		echo "${CYAN}Environment Variables Related with Network Proxy: ${RESET}"
+		echo $proxy_env | while read line; do echo "${INDENT}${line}"; done
+		echo
+		echo "${YELLOW}VPN Client Status: ${RESET}"
+		if [[ $(uname -r | grep 'WSL2') ]]; then
+			warning "Unknown. For WSL2, the VPN client is probably running on the host machine. Please check manually."
+		elif [[ -f /.dockerenv ]]; then
+			warning "Unknown. For a Docker container, the VPN client is probably running on the host machine. Please check manually."
+		else
+			echo "${INDENT}$(systemctl is-active sing-box-${PROTOCOL}.service)"
+		fi
+		echo
+	fi
 }
 
-printf "${PURPLE}Avaiable handy commands for networking:${RESET}
+printf "
+${MAGENTA}Avaiable handy commands for networking proxy (protocol: ${PROTOCOL}):${RESET}
 
 ${INDENT}${GREEN}${BOLD}\$${RESET} set_proxy
 ${INDENT}${GREEN}${BOLD}\$${RESET} unset_proxy
+
 ${INDENT}${GREEN}${BOLD}\$${RESET} check_private_ip
 ${INDENT}${GREEN}${BOLD}\$${RESET} check_public_ip
 ${INDENT}${GREEN}${BOLD}\$${RESET} check_proxy_status
